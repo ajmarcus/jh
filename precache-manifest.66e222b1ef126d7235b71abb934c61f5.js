154a4b1cab01fb239a0a77c82fa0fcd16dadfe26
Ariel Marcus <hi@arielmarcus.com>
2020-06-25 23:39:22 -0400

    Updates

---
+++ precache-manifest.66e222b1ef126d7235b71abb934c61f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6b817e76a28155947575c8c8b587f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d70038cb41f1400af056",
+    "url": "/static/css/main.7554be03.chunk.css"
+  },
+  {
+    "revision": "d358c9b5932b77598e10",
+    "url": "/static/js/2.e7f282b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e7f282b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70038cb41f1400af056",
+    "url": "/static/js/main.3a34b07d.chunk.js"
+  },
+  {
+    "revision": "d554af21e14ef837ab0f",
+    "url": "/static/js/runtime-main.b8e796d8.js"
+  }
+]);
