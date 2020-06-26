deb490616b165e1b9bf2ceb57cd70efda83ce265
Ariel Marcus <hi@arielmarcus.com>
2020-06-26 01:16:48 -0400

    airtable node

--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import data from './components/data.js';
+import Airtable from 'airtable';
 
+let base = new Airtable({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: 'keyP2OBqwhjW6HpxS'
+}).base('app9W8uIpC5sD7e40');
 console.log(data['records'][0]);
+
+base('LEARN').select({
+  // Selecting the first 3 records in Full List (Table):
+  maxRecords: 3,
+  view: "Full List (Table)"
+}).eachPage(function page(records, fetchNextPage) {
+  // This function (`page`) will get called for each page of records.
+
+  records.forEach(function (record) {
+    console.log('Retrieved', record.get('Resource'));
+  });
+
+  // To fetch the next page of records, call `fetchNextPage`.
+  // If there are more records, `page` will get called again.
+  // If there are no more records, `done` will get called.
+  fetchNextPage();
+
+}, function done(err) {
+  if (err) { console.error(err); return; }
+});
 
 ReactDOM.render(
   <React.StrictMode>
