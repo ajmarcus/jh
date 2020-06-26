7ab339d7d3522b2c8e11fc70683f2907a1a74e67
Ariel Marcus <hi@arielmarcus.com>
2020-06-26 00:12:23 -0400

    airtable data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import data from './components/data.js';
+
+console.log(data['records'][0]);
 
 ReactDOM.render(
   <React.StrictMode>
